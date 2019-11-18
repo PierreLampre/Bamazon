0ef3d48019491f1334a6f9c0489b0fc1463fe68a
Justin Lampe <pierrelampre@gmail.com>
2019-11-17 23:30:24 -0500

    used inquirer to take in a number. if the number matches an id of an item on in the table, an additional inquirer takes in the quantity desired to be purchased, updates the inventory, gives the customer the total for their bill.

--- bamazon.js
+++ bamazon.js
@@ -24,12 +24,83 @@ connection.connect(function (err) {
 
 function start() {
     console.log("-|- -|- Welcome to Bamazon -|- -|-\n");
-    connection.query("SELECT * FROM inventory", function(err, res) {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-     
-      console.table(res);
+    connection.query("SELECT * FROM inventory", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
 
-      connection.end();
+        console.table(res);
+
+        askForId();
+    });
+}
+
+function askForId() {
+    connection.query("SELECT * FROM inventory", function (err, res) {
+        if (err) throw err;
+
+        inquirer.prompt([
+
+            {
+                type: "input",
+                name: "theID",
+                message: "Which product would you like to purchase? (Enter ID# - Or Q to Quit)"
+            }
+
+        ]).then(function (answer) {
+
+
+            if (answer.theID === "q") {
+                connection.end();
+            }
+
+
+            var chosenItem;
+
+            for (var i = 0; i < res.length; i++) {
+              if (res[i].id == answer.theID) {
+                chosenItem = res[i];
+              }
+            //   } else {
+            //       console.log("**--__INVALID ENTRY - Please Try Again**--__");
+            //       askForId();
+            //   }
+            }
+
+            inquirer.prompt([
+
+                {
+                    type: "input",
+                    name: "count",
+                    message: "How many units would you like to purchase?"
+                }
+
+
+            ]).then(function (answer) {
+
+                let newStock = chosenItem.stock - parseInt(answer.count);
+
+                connection.query(
+                    "UPDATE inventory SET ? WHERE ?",
+                    [
+                      {
+                        stock: newStock
+                      },
+                      {
+                        id: chosenItem.id
+                      }
+                    ],
+                    function(error) {
+                      if (error) throw err;
+                      console.log("");
+                      console.log("Thank you for your purchase! Your total is $" + chosenItem.price * answer.count + ". Thank you for choosing Bamazon.");
+                      console.log("");
+                      start();
+                    }
+                  );
+
+
+            });
+        })
+
     });
 }
